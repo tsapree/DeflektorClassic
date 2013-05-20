ec0edfaf11093b34b1eb8d67b9b9de08916ea305
tsap <tsap@notebook>
2013-05-21 00:33:24 +0600

    LEVELS 8-10

--- src/pro/oneredpixel/deflektorclassic/Deflektor.java
+++ src/pro/oneredpixel/deflektorclassic/Deflektor.java
@@ -40,7 +40,7 @@ public class Deflektor implements ApplicationListener {
 	int panScale;
 	
 	int playingLevel = 1;
-	int unlockedLevel = 7;
+	int unlockedLevel = 12;
 	final int countOfLevels = 60;
 
 
